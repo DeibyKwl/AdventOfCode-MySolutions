299a71aaddbf3c8d53cf0d45e6ee36d53f32fcf7
deibywu <deiby.wu@maine.edu>
2023-11-11 15:18:19 -0500

    Day2

---
+++ 2022/Day2/challenge1.py
@@ -0,0 +1,13 @@
+# What would your total score be if everything goes exactly according to your strategy guide?
+
+import os
+
+print("Current working directory:", os.getcwd())
+
+"""file_path = os.path.join('AdventOfCode','2022','Day2', 'input.txt')
+
+with open(file_path, 'r') as f:
+    text = f.readlines()
+
+for line in text:
+    print(line, end='')"""
