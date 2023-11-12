6cda5b1331ef267aa21ee8ad896351ee3877b550
deibywu <deiby.wu@maine.edu>
2023-11-11 19:21:30 -0500

    Update challenge1.py

--- 2022/Day2/challenge1.py
+++ 2022/Day2/challenge1.py
@@ -1,13 +1,39 @@
 # What would your total score be if everything goes exactly according to your strategy guide?
+# A,B,C: Rock, Paper, Scissor
+# X,Y,Z: Rock, Paper, Scissor
 
-import os
+total_sum = 0
+text = ''
+opponent_moves = ['A', 'B', 'C']
+player_moves = {'X':1, 'Y':2, 'Z':3}
 
-print("Current working directory:", os.getcwd())
-
-"""file_path = os.path.join('AdventOfCode','2022','Day2', 'input.txt')
-
-with open(file_path, 'r') as f:
+# Read the text file and save it into text variable
+with open('input.txt', 'r') as f:
     text = f.readlines()
-
 for line in text:
-    print(line, end='')"""
+    opponent, player = line.split(' ')
+    opponent, player = opponent.strip(), player.strip()
+
+    # Tie
+    if opponent_moves.index(opponent) == list(player_moves).index(player):
+        total_sum += (3 + player_moves[str(player)])
+        
+    elif player == 'X': # Rock
+        if opponent == 'B': #Paper
+            total_sum += (0 + player_moves[str(player)])
+        else: #Scissor
+            total_sum += (6 + player_moves[str(player)])
+            
+    elif player == 'Y': # Paper
+        if opponent == 'A': # Rock
+            total_sum += (6 + player_moves[str(player)])
+        else: # Scissor
+            total_sum += (0 + player_moves[str(player)])
+
+    elif player == 'Z': # Scissor 
+        if opponent == 'B': # Paper
+            total_sum += (6 + player_moves[str(player)])
+        else: # Rock
+            total_sum += (0 + player_moves[str(player)])
+
+print(total_sum)
